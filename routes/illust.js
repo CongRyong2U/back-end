2adf4f9a5de8940b649c040a4be141c7c3bebc6b
unknown <lerle144@gmail.com>
2023-01-25 06:53:18 +0900

    api 작성 illust/list, illust/detail

--- routes/illust.js
+++ routes/illust.js
@@ -2,13 +2,33 @@ const express = require("express");
 const router = express.Router();
 const { Illust } = require("../models/Illust");
 
-router.get("/list", (req, res) => {
+router.get("/list", async (req, res, next) => {
+    try{
+        const illustList = await Illust.find({}, {desc: 0});
+        return res.status(200).json({
+            success: true,
+            illustList
+        });
 
+        } catch (err) {
+        res.json({ success: false, err });
+        next(err);
+    }
   
 });
 
-router.get("/detail/:illustId", (req, res) => {
+router.get("/detail/:illustId", async (req, res, next) => {
+    try{
+        const illust = await Illust.findOne({_id: req.params.illustId});
+        return res.status(200).json({
+            success: true,
+            illust
+        });
 
+        } catch (err) {
+        res.json({ success: false, err });
+        next(err);
+    }
   
 });
 
