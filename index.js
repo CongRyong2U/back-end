eac11cff26a410b8197eb9b32190069cb1202765
unknown <lerle144@gmail.com>
2023-02-06 12:19:50 +0900

    cors 추가

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require('cors');
 const app = express(); // 새 앱을 만듦
+app.use(cors());
 const port = process.env.PORT || 5000; // 백 서버 포트 설정
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser"); // 로그인 토큰을 쿠키에 저장하기
@@ -25,6 +27,8 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/stage", require("./routes/stage"));
 app.use("/api/game", require("./routes/game"));
 app.use("/api/illust", require("./routes/illust"));
+
+
 
 app.listen(port, () => { // 포트(5000)에서 실행(listen)
     console.log(`Example app listening on port ${port}`); // npm run start하면 터미널 콘솔에 출력
